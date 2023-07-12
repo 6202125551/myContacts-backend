227981caa92414d2a134cf583a3beb92d7087aa0
Ratnaraj Anand <anand.ratnaraj@gmail.com>
2023-07-13 01:04:09 +0530

    created the all four routes i.e get post put delete

---
+++ routes/contactRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+    res.status(200).json({message : "get all contacts"});
+})
+
+router.post('/', (req, res) => {
+
+    res.status(200).json({message :" i am in post"});
+})
+
+router.put('/:id', (req, res) => {
+
+    res.status(200).json({message: `update the contact with id ${req.params.id}`});
+})
+
+router.delete('/:id', (req, res) => {
+
+    res.status(200).json({message : `delete the contact with id ${req.params.id}`});
+})
+
+
+module.exports = router;
