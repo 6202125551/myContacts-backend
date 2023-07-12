46e81e42429a3293a7a239105004535d2da2994d
Ratnaraj Anand <anand.ratnaraj@gmail.com>
2023-07-13 04:19:18 +0530

    validation and express-async-handler concept

---
+++ middleware/errorHandler.js
@@ -0,0 +1,31 @@
+const {constants} = require('../constants')
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    switch(statusCode){
+        case constants.VALIDATION_ERROR:
+            res.json({title:"VALIDATION ERROR",message: err.message, stackTrace: err.stack})
+            break;
+        case constants.NOT_FOUND:
+            res.json({title:"NOT FOUND",message: err.message, stackTrace: err.stack})
+            break;
+        case constants.UNAUTHORISED:
+            res.json({title:"UNAUTHORIESD",message: err.message, stackTrace: err.stack})
+            break;
+        case constants.FORBIDDEN:
+            res.json({title:"FORBIDDEN",message: err.message, stackTrace: err.stack})
+            break;
+        case constants.SERVER_ERROR:
+            res.json({title:"SERVER ERROR",message: err.message, stackTrace: err.stack})
+            break;
+        default :
+
+            break;
+    }
+
+    
+    
+
+}
+
+
+module.exports = errorHandler;
