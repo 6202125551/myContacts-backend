91ebb8a3acd3ba12baa14df7273d503cf21ad584
Ratnaraj Anand <anand.ratnaraj@gmail.com>
2023-07-13 05:40:10 +0530

    mongodb setup and CRUD OPEARATION IN DATABASE through api

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const dotenv = require('dotenv').config()
 const express = require('express');
 const errorHandler = require('./middleware/errorHandler');
+const connectDb = require('./config/dbConnection');
 
-
+connectDb();
 const app = express()
 const port = process.env.PORT || 5000
 
